d1f5ea8fb35c8ddc29ac393ee97551800c5ff157
kkopczyk <ken.kopczyk@gmail.com>
2013-11-09 18:58:51 -0500

    Integrated vector D3 graphics

---
+++ Hackathon/Rest/Services/MiserableService.cs
@@ -0,0 +1,65 @@
+﻿using Hackathon.Rest.Endpoints;
+using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Hackathon.Rest.Services
+{
+    public class MiserableService : Service
+    {
+        public object Get(MiserableSampleRequest request)
+        {
+            return @"{
+  ""nodes"":[
+    {""name"":""Myriel"",""group"":1},
+    {""name"":""Napoleon"",""group"":1},
+    {""name"":""Mlle.Baptistine"",""group"":1},
+    {""name"":""Mme.Magloire"",""group"":1},
+    {""name"":""CountessdeLo"",""group"":1},
+    {""name"":""Geborand"",""group"":1},
+    {""name"":""Champtercier"",""group"":1},
+    {""name"":""Cravatte"",""group"":1},
+    {""name"":""Count"",""group"":1},
+    {""name"":""OldMan"",""group"":1},
+    {""name"":""Labarre"",""group"":2},
+    {""name"":""Valjean"",""group"":2},
+    {""name"":""Marguerite"",""group"":3},
+    {""name"":""Mme.deR"",""group"":2},
+    {""name"":""Isabeau"",""group"":2},
+    {""name"":""Gervais"",""group"":2},
+    {""name"":""Tholomyes"",""group"":3},
+    {""name"":""Listolier"",""group"":3},
+    {""name"":""Fameuil"",""group"":3}
+  ],
+  ""links"":[
+    {""source"":1,""target"":0,""value"":1},
+    {""source"":2,""target"":0,""value"":8},
+    {""source"":3,""target"":0,""value"":10},
+    {""source"":3,""target"":2,""value"":6},
+    {""source"":4,""target"":0,""value"":1},
+    {""source"":5,""target"":0,""value"":1},
+    {""source"":6,""target"":0,""value"":20},
+    {""source"":7,""target"":0,""value"":1},
+    {""source"":8,""target"":0,""value"":2},
+    {""source"":9,""target"":0,""value"":1},
+    {""source"":11,""target"":10,""value"":1},
+    {""source"":11,""target"":3,""value"":3},
+    {""source"":11,""target"":2,""value"":3},
+    {""source"":11,""target"":0,""value"":5},
+    {""source"":12,""target"":11,""value"":1},
+    {""source"":13,""target"":11,""value"":1},
+    {""source"":14,""target"":11,""value"":1},
+    {""source"":15,""target"":11,""value"":1},
+    {""source"":17,""target"":16,""value"":4},
+    {""source"":18,""target"":16,""value"":4},
+    {""source"":18,""target"":17,""value"":4}
+  ]
+}";
+        }
+
+    }
+}
--- Hackathon/js/ponies.js
+++ Hackathon/js/ponies.js
@@ -84,8 +84,157 @@ function doSearch() {
 
 function dunsLinkClick(e) {
     e.preventDefault();
-    var selectedDuns = $(e.target).attr('href');
+    //var selectedDuns = $(e.target).attr('href');
 
     $('#content').html('<div id="loading"><img /></div>');
-    $('<div>' + selectedDuns + '</div>').appendTo($('#content'));
+    $.when(GetMiserableSample()).then(
+        function (data, textStatus, jqXHR) {
+            initializeGraphPage();
+        },
+        function (jqXHR, textStatus, errorThrown) {
+            $('#content').html("Miserable Sample Failure");
+        }
+    );
+
+    //$('<div>' + selectedDuns + '</div>').appendTo($('#content'));
+}
+
+function GetMiserableSample() {
+    return $.ajax({
+        url: "/miserable",
+        type: "GET",
+        dataType: "json"
+    });
+}
+
+
+
+
+
+
+
+
+
+function name(d) { return d.name; }
+function group(d) { return d.group; }
+
+var color = d3.scale.category10();
+function colorByGroup(d) { return color(group(d)); }
+
+var width = 1260,
+    height = 800;
+
+//var svg = d3.select('#viz')
+//    .append('svg')
+//    .attr('width', width)
+//    .attr('height', height);
+var svg = null;
+
+var node, link;
+
+var voronoi = d3.geom.voronoi()
+    .x(function (d) { return d.x; })
+    .y(function (d) { return d.y; })
+    .clipExtent([[-10, -10], [width + 10, height + 10]]);
+
+function recenterVoronoi(nodes) {
+    var shapes = [];
+    voronoi(nodes).forEach(function (d) {
+        if (!d.length) return;
+        var n = [];
+        d.forEach(function (c) {
+            n.push([c[0] - d.point.x, c[1] - d.point.y]);
+        });
+        n.point = d.point;
+        shapes.push(n);
+    });
+    return shapes;
+}
+
+var force = d3.layout.force()
+    .charge(-2000)
+    .friction(0.3)
+    .linkDistance(50)
+    .size([width, height]);
+
+
+function registerTick() {
+    force.on('tick', function () {
+        node.attr('transform', function (d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+            .attr('clip-path', function (d) { return 'url(#clip-' + d.index + ')'; });
+
+        link.attr('x1', function (d) { return d.source.x; })
+            .attr('y1', function (d) { return d.source.y; })
+            .attr('x2', function (d) { return d.target.x; })
+            .attr('y2', function (d) { return d.target.y; });
+
+        var clip = svg.selectAll('.clip')
+            .data(recenterVoronoi(node.data()), function (d) { return d.point.index; });
+
+        clip.enter().append('clipPath')
+            .attr('id', function (d) { return 'clip-' + d.point.index; })
+            .attr('class', 'clip');
+        clip.exit().remove()
+
+        clip.selectAll('path').remove();
+        clip.append('path')
+            .attr('d', function (d) { return 'M' + d.join(',') + 'Z'; });
+    });
+}
+
+
+var mouseOverEventHandler = function (event) {
+
+    //alert('bam');
+    var x = 5;
+}
+
+
+
+function initializeGraphPage() {
+
+    $('#content').html("<div id='viz'></div>");
+    svg = d3.select('#viz')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
+    registerTick();
+    d3.json('/miserable', function (err, data) {
+
+        data.nodes.forEach(function (d, i) {
+            d.id = i;
+        });
+
+        link = svg.selectAll('.link')
+            .data(data.links)
+          .enter().append('line')
+            .attr('class', 'link')
+            .style("stroke-width", function (d) { return d.value; });
+        //.style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+        node = svg.selectAll('.node')
+            .data(data.nodes)
+          .enter().append('g')
+            .attr('title', name)
+            .attr('class', 'node')
+            .call(force.drag)
+
+
+        //  breast
+        node.append('circle')
+            .attr('r', 40)
+            .attr('fill', colorByGroup)
+            .attr('fill-opacity', 0.9)
+            .attr('onmouseover', 'mouseOverEventHandler(this)');
+
+        //  nipple
+        node.append('circle')
+            .attr('r', 8)
+            .attr('stroke', 'black');
+
+        force
+            .nodes(data.nodes)
+            .links(data.links)
+            .start();
+    });
 }
