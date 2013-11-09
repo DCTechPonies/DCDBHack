29055c60a0eca6e0b6fd307658226a2fcd5c5923
jwilson <jwilson@opisnet.com>
2013-11-09 15:47:16 -0500

    blargh

--- Hackathon/Rest/Services/DnbCompanyService.cs
+++ Hackathon/Rest/Services/DnbCompanyService.cs
@@ -18,10 +18,6 @@ namespace Hackathon.Rest.Services
             //Prod
             request.AuthToken = "AsAFLo/h3hh3qyjaLwzpYv3usddOE6XqGM7sDWV+/mbJeduCr1TnkX0QA/x/MEAE8JiiDKbQSVvNxWf/A9qfni8OaS83DGzdoDmzPFPfsYlF8SwCgTxnOgrR+PLPuYHd/dnIA7aSNk/BZ9WCAKxmdstVoGW+L3cTtX6lx92zko1lAZ/JsDYDRHMOSnX1jqXnIzf4WPjKFJipztzOM+U1Khm6qIkUJUVTiLtH+9ev590yKnPHTYSeoz5ULdJpCcOdFvRBuRidgtPtxjvsLIA/aEQqZWZlWueq/E2iHhy5LZ87lzd89zTPBN7JGS6qidGPwxlbQ/EhEk2X9cDY1LPiAQ==";
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
             var restClient = new RestClient("https://maxcvservices.dnb.com/V6.0");
             var restRequest = new RestRequest();
             switch (request.SearchTerms)
@@ -46,10 +42,7 @@ namespace Hackathon.Rest.Services
 		        default:
                     throw new Exception(request.SearchTerms + " is not a vaild entity type.");
 	        }
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
             restRequest.AddHeader("Authorization", request.AuthToken);
             var response = restClient.Execute(restRequest);
             if (response.StatusCode != System.Net.HttpStatusCode.OK)
