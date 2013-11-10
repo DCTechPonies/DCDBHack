02294b989f6d56b736ab24eb270074876ee0fd76
Mohsin <mohsinmajid@gmail.com>
2013-11-09 19:02:41 -0500

    added Data Exchange service
    for social stuff

---
+++ Hackathon/Rest/Endpoints/DnbDataExchangeRequest.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackathon.Rest.Endpoints
+{
+    [Route("/dnb/social/{filter}/{searchTerms}", "GET")]
+    public class DnbDataExchangeRequest
+    {
+        public string AuthToken { get; set; }
+        public string Filter { get; set; }
+        public string SearchTerms { get; set; }
+
+    }
+}
---
+++ Hackathon/Rest/Services/DnbDataExchangeService.cs
@@ -0,0 +1,53 @@
+﻿using Hackathon.Rest.Endpoints;
+using ServiceStack.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Hackathon.Rest.Services
+{
+    public class DnbDataExchangeService : Service
+    {
+        public object Get(DnbDataExchangeRequest request)
+        {
+            //Sandbox
+            //request.AuthToken = "OLtSGKF9hEIePfGG45cjgpSSLIAk2cQcTJkc7i8Saw0tTiODz2NAUdyGUsKOsNmIHm5Jw/SsbjoycpFfMyhPoJS2NsIhOFZWYwZsO+3dfXlzlBvoLkAxP020zScurR6wnSKozn6np6rLhpE0ejUVTvG8iKCHywP9dyPgg1vXRX1o1YbX8XMlSpa+VAWhJVAiRRNuMmIlATJLamrhxSTJ1jgydRbdZ80Jm4llCRzOQCb/9CHkyKrt0AgQckb/X6nsR80ydiOqFRavtRLjK3X+qgRX9DRcaHLcWPGRWhWt8MMBt11zijUswuk8Z4UaocY9ZA8nSUWdiebbcpBy0hHxuvDIG4dZByNRBkWc6EYNXHI=";
+            //Prod
+            request.AuthToken = "AsAFLo/h3hh3qyjaLwzpYv3usddOE6XqGM7sDWV+/mbJeduCr1TnkX0QA/x/MEAE8JiiDKbQSVvNxWf/A9qfni8OaS83DGzdoDmzPFPfsYlF8SwCgTxnOgrR+PLPuYHd/dnIA7aSNk/BZ9WCAKxmdstVoGW+L3cTtX6lx92zko1lAZ/JsDYDRHMOSnX1jqXnIzf4WPjKFJipztzOM+U1Khm6qIkUJUVTiLtH+9ev590yKnPHTYSeoz5ULdJpCcOdFvRBuRidgtPtxjvsLIA/aEQqZWZlWueq/E2iHhy5LZ87lzd89zTPBN7JGS6qidGPwxlbQ/EhEk2X9cDY1LPiAQ==";
+
+            var restClient = new RestClient("https://maxcvservices.dnb.com/dataexchange/fliptop");
+            var restRequest = new RestRequest();
+
+            switch (request.Filter)
+            {
+                case "company":
+                    restRequest.Method = Method.GET;
+                    restRequest.Resource = String.Format("/beta/company?duns={0}", request.SearchTerms);
+                    break;
+                case "person":
+                    restRequest.Method = Method.GET;
+                    restRequest.Resource = String.Format("/beta/person?email={0}", request.SearchTerms);
+                    break;
+                default:
+                    throw new Exception(request.Filter + " is not a vaild type.");
+            }
+
+            restRequest.AddHeader("Authorization", request.AuthToken);
+            var response = restClient.Execute(restRequest);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new HttpException(500, "D&B Request Failed.");
+            }
+            //var responseObject = JsonConvert.DeserializeObject<Rootobject>(response.Content);
+            return response.Content;
+        }
+
+
+
+
+
+    }
+}
