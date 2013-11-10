128f4b469d223c233cc2eee2e164063668a88778
jwilson <jwilson@opisnet.com>
2013-11-10 01:06:05 -0500

    Added Financial Statement service

--- Hackathon/Rest/Endpoints/DnbFinancialRequest.cs
+++ Hackathon/Rest/Endpoints/DnbFinancialRequest.cs
@@ -6,8 +6,8 @@ using System.Web;
 
 namespace Hackathon.Rest.Endpoints
 {
-    [Route("/dnb/firmographic/{_searchTerms}", "GET")]
-    public class DnbFirmographicRequest : IGenericRequest
+    [Route("/dnb/financial/{_searchTerms}", "GET")]
+    public class DnbFinancialRequest : IGenericRequest
     {
     }
 }
